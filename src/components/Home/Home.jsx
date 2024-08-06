916d1b50cac991d9ead52fe65fed3afc9a1b89aa
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-08-07 01:34:29 +0600

    error boundary aded with react-error-boundary

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,23 @@
 import { Suspense } from "react";
 import Posts from "./Posts";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Home = () => {
   return (
     <div>
-      <Suspense fallback={<div className="text-7xl "> Loading ... </div>}>
-        <Posts />
-      </Suspense>
+      <ErrorBoundary
+        fallback={
+          <>
+            <div>
+              <h1 className="text-7xl text-red-500 text-center">Something went wrong</h1>
+            </div>
+          </>
+        }
+      >
+        <Suspense fallback={<div className="text-7xl "> Loading ... </div>}>
+          <Posts />
+        </Suspense>
+      </ErrorBoundary>
     </div>
   );
 };
